0ed617f8ba57e160351b23b0e2b2e53b8c12e423
Rose Fan <ruosi.fan@gmail.com>
2012-04-29 20:45:53 -0500

    added even more tests!

--- src/NaturalNumsTest.java
+++ src/NaturalNumsTest.java
@@ -1,16 +1,29 @@
 import static org.junit.Assert.*;
-
 import org.junit.Test;
-
-
 
 public class NaturalNumsTest {
 
-
 	@Test
-	public void shouldFindAllNumsDivisibleByThreeOrFive(){
+	public void shouldReturnFalse(){
 		NaturalNums nn = new NaturalNums();
-		assertEquals(nn.run(10), 3+5+6+9);
+		assertEquals(false, nn.isItDivisible(11));
+	}
+	
+	@Test
+	public void shouldReturnTrue(){
+		NaturalNums nn = new NaturalNums();
+		assertEquals(true, nn.isItDivisible(36));
+	}
+	
+	@Test
+	public void shouldFindAllNumsDivisibleByThreeOrFiveUnderTen(){
+		NaturalNums nn = new NaturalNums();
+		assertEquals(3+5+6+9, nn.run(10));
 	}
 
+	@Test
+	public void shouldReturnZero(){
+		NaturalNums nn = new NaturalNums();
+		assertEquals(0, nn.run(1));
+	}
 }
